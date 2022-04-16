c66f401efca9a48c0ba0b105641fb933533b89be
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-04-16 20:03:14 -0300

    Fix - testes unitários no componente Banking

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -19,7 +19,29 @@ describe('BankingComponent', () => {
     fixture.detectChanges();
   });
 
+  //componente deverá ser criado
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // U => teste unitário
+  it(`(U) getPoupanca: should have poupanca = 10`, () => {
+    expect(component.getPoupanca).toEqual(10);
+  });
+
+  it(`(U) getCarteira(): should have carteira = 50`, () => {
+    expect(component.getCarteira).toEqual(50);
+  });
+
+  it(`(U) setSacar(): should transfer poupanca from carteira`, () => {
+    component.setSacar('10');    
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  });
+
+  it(`(U) setDepositar(): should transfer carteira from poupanca`, () => {
+    component.setDepositar('50');    
+    expect(component.getCarteira).toEqual(0);
+    expect(component.getPoupanca).toEqual(60);
   });
 });
