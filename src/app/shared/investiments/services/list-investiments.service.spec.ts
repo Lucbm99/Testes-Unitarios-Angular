5280db9a2f3d1613a4d2b86a299d4d1e37217e05
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-04-19 07:18:06 -0300

    Fix - configurando teste de serviço

--- src/app/shared/investiments/services/list-investiments.service.spec.ts
+++ src/app/shared/investiments/services/list-investiments.service.spec.ts
@@ -1,15 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ListInvestimentsService } from './list-investiments.service';
+import { 
+  HttpClientTestingModule, 
+  HttpTestingController 
+} from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('ListInvestimentsService', () => {
   let service: ListInvestimentsService;
+  let httpTestingController: HttpTestingController;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ListInvestimentsService);
   });
 
+  httpClient = TestBed.inject(HttpClient);
+  httpTestingController = TestBed.inject(HttpTestingController);
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
